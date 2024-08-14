30eb76b50f68f5925052f840b51ec5fb9fda70bb
skykongkong8 <ss.kong@samsung.com>
2024-08-14 15:38:23 +0900

    [ RESET THIS COMMIT ]
    add ARMV7 option and add armv7 accordingly for *neon.cpp files

--- nntrainer/tensor/cpu_backend/arm/armv7_neon.h
+++ nntrainer/tensor/cpu_backend/arm/armv7_neon.h
@@ -2,8 +2,14 @@
 #include <cmath>
 
 #define vfmaq_n_f32(a, b, c, n) vaddq_f32(a, vmulq_f32(b, vmovq_n_f32(c[n])))
-#define vfmaq_n_f16(a, b, c, n) vaddq_f16(a, vmulq_f16(b, vmovq_n_f16(c[n])))
 
+/**
+ * @brief vdivq_f32 macro
+ * 
+ * @param a a for a / b
+ * @param b b for a / b
+ * @return float32x4_t 
+ */
 static inline float32x4_t vdivq_f32(float32x4_t a, float32x4_t b) {
   float32x4_t ret;
   for (unsigned int i = 0; i < 4; ++i) {
@@ -13,7 +19,13 @@ static inline float32x4_t vdivq_f32(float32x4_t a, float32x4_t b) {
 }
 
 // vsqrtq_f32
-static inline float32x4_t vdivq_f32(float32x4_t a) {
+/**
+ * @brief vsqrtq_f32 macro
+ * 
+ * @param a input vector
+ * @return float32x4_t 
+ */
+static inline float32x4_t vsqrtq_f32(float32x4_t a) {
   float32x4_t ret;
   for (unsigned int i = 0; i < 4; ++i) {
     ret[i] = std::sqrt(a[i]);
@@ -22,28 +34,52 @@ static inline float32x4_t vdivq_f32(float32x4_t a) {
 }
 
 // vmaxvq_f32
-static inline float32_t vmaxvq_f32(float32x4_t a) {
-  float32_t ret = a[0];
+/**
+ * @brief vmaxvq_f32 macro
+ * 
+ * @param a input vector
+ * @return float 
+ */
+static inline float vmaxvq_f32(float32x4_t a) {
+  float ret = a[0];
   for (unsigned int i = 1; i < 4; ++i) {
     if (ret > a[i])
-      ? ret = a[i] : continue;
+      ret = a[i];
   }
+  return ret;
 }
 
+// vaddvq_f32
+/**
+ * @brief vaddvq_f32
+ * 
+ * @param a input vector
+ * @return float32_t 
+ */
+static inline float32_t vaddvq_f32(float32x4_t a) {
+  float32_t ret = a[0];
+  for (unsigned int i = 1; i < 4; ++i) {
+    ret += a[i];
+  }
+  return ret;
+}
+
+#ifdef ENABLE_FP16
+#define vfmaq_n_f16(a, b, c, n) vaddq_f16(a, vmulq_f16(b, vmovq_n_f16(c[n])))
+
 // vmaxvq_f16
+/**
+ * @brief vmaxvq_f16 macro
+ * 
+ * @param a input vector
+ * @return float16_t 
+ */
 static inline float16_t vmaxvq_f16(float16x8_t a) {
   float16_t ret = a[0];
   for (unsigned int i = 1; i < 8; ++i) {
     if (ret > a[i])
-      ? ret = a[i] : continue;
+      ret = a[i];
   }
+  return ret;
 }
-
-// vaddvq_f32
-static inline float32_t vaddvq_f32(float32x4_t a){
-    float32_t ret = a[0];
-    for (unsigned int i = 1; i < 4; ++i){
-        ret += a[i];
-    }
-    return ret;
-}
+#endif
