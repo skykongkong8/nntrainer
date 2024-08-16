fa747aab8190ee9c9b1d23e27e4182bd101bc877
skykongkong8 <ss.kong@samsung.com>
2024-08-16 09:39:29 +0900

    [ RESET THIS COMMIT ] Test commit to set commit message template
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/cpu_backend/arm/armv7_neon.h
+++ nntrainer/tensor/cpu_backend/arm/armv7_neon.h
@@ -1,14 +1,18 @@
 #include <arm_neon.h>
 #include <cmath>
 
+/**
+ * @brief macro for vfmaq_n_f32
+ * 
+ */
 #define vfmaq_n_f32(a, b, c, n) vaddq_f32(a, vmulq_f32(b, vmovq_n_f32(c[n])))
 
 /**
  * @brief vdivq_f32 macro
- * 
+ *
  * @param a a for a / b
  * @param b b for a / b
- * @return float32x4_t 
+ * @return float32x4_t
  */
 static inline float32x4_t vdivq_f32(float32x4_t a, float32x4_t b) {
   float32x4_t ret;
@@ -18,12 +22,11 @@ static inline float32x4_t vdivq_f32(float32x4_t a, float32x4_t b) {
   return ret;
 }
 
-// vsqrtq_f32
 /**
  * @brief vsqrtq_f32 macro
- * 
+ *
  * @param a input vector
- * @return float32x4_t 
+ * @return float32x4_t
  */
 static inline float32x4_t vsqrtq_f32(float32x4_t a) {
   float32x4_t ret;
@@ -33,12 +36,11 @@ static inline float32x4_t vsqrtq_f32(float32x4_t a) {
   return ret;
 }
 
-// vmaxvq_f32
 /**
  * @brief vmaxvq_f32 macro
- * 
+ *
  * @param a input vector
- * @return float 
+ * @return float
  */
 static inline float vmaxvq_f32(float32x4_t a) {
   float ret = a[0];
@@ -49,12 +51,11 @@ static inline float vmaxvq_f32(float32x4_t a) {
   return ret;
 }
 
-// vaddvq_f32
 /**
  * @brief vaddvq_f32
- * 
+ *
  * @param a input vector
- * @return float32_t 
+ * @return float32_t
  */
 static inline float32_t vaddvq_f32(float32x4_t a) {
   float32_t ret = a[0];
@@ -65,14 +66,17 @@ static inline float32_t vaddvq_f32(float32x4_t a) {
 }
 
 #ifdef ENABLE_FP16
+/**
+ * @brief macro for vfmaq_n_f16
+ *
+ */
 #define vfmaq_n_f16(a, b, c, n) vaddq_f16(a, vmulq_f16(b, vmovq_n_f16(c[n])))
 
-// vmaxvq_f16
 /**
  * @brief vmaxvq_f16 macro
- * 
+ *
  * @param a input vector
- * @return float16_t 
+ * @return float16_t
  */
 static inline float16_t vmaxvq_f16(float16x8_t a) {
   float16_t ret = a[0];
