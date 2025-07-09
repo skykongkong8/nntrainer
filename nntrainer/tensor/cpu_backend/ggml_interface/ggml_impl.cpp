27f0ef3fe8e7c5d42fe5888491c0770ce92c6c72
skykongkong8 <ss.kong@samsung.com>
2025-07-09 16:19:53 +0900

    [ ggml ] Implement __nntr_quantize_mat_q8_0_4x8
    - FP16 version of matrix-wise quantization for runtime quantization of q4_0_4x8 GEMM
    - Should be tested with GEMM
    
    **Self evaluation:**
    1. Build test:     [X]Passed [ ]Failed [ ]Skipped
    2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: skykongkong8 <ss.kong@samsung.com>

--- nntrainer/tensor/cpu_backend/ggml_interface/ggml_impl.cpp
+++ nntrainer/tensor/cpu_backend/ggml_interface/ggml_impl.cpp
@@ -6,35 +6,34 @@
 #include <algorithm>
 #include <assert.h>
 #include <cmath>
+#include <cstring>
 #include <ggml_interface.h>
 #include <iostream>
 #include <math.h>
 #include <stdint.h>
-#include <cstring>
 #if defined(__ARM_NEON)
 #include <arm_neon.h>
 #endif
 
 #ifndef MAX
-#define MAX(x,y) (((x) > (y)) ? (x) : (y))
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #endif
 #ifndef MIN
-#define MIN(x,y) (((x) < (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #endif
 
 typedef struct {
-    GGML_EXTENSION union {
-        struct {
-            ggml_half d;    // super-block scale for quantized scales
-            ggml_half dmin; // super-block scale for quantized mins
-        } GGML_COMMON_AGGR_S;
-        ggml_half2 dm;
-    } GGML_COMMON_AGGR_U;
-    uint8_t scales[K_SCALE_SIZE]; // scales and mins, quantized with 6 bits
+  GGML_EXTENSION union {
+    struct {
+      ggml_half d;    // super-block scale for quantized scales
+      ggml_half dmin; // super-block scale for quantized mins
+    } GGML_COMMON_AGGR_S;
+    ggml_half2 dm;
+  } GGML_COMMON_AGGR_U;
+  uint8_t scales[K_SCALE_SIZE]; // scales and mins, quantized with 6 bits
 } q4_K_qparams;
 namespace nntrainer {
 
-  
 template <int K> constexpr int QK_0() {
   if constexpr (K == 4) {
     return QK4_0;
@@ -65,20 +64,24 @@ static inline uint16_t nntr_compute_fp32_to_fp16(float f) {
   return res;
 }
 
-static inline void get_scale_min_k4(int j, const uint8_t * GGML_RESTRICT q, uint8_t * GGML_RESTRICT d, uint8_t * GGML_RESTRICT m) {
-    if (j < 4) {
-        *d = q[j] & 63; *m = q[j + 4] & 63;
-    } else {
-        *d = (q[j+4] & 0xF) | ((q[j-4] >> 6) << 4);
-        *m = (q[j+4] >>  4) | ((q[j-0] >> 6) << 4);
-    }
+static inline void get_scale_min_k4(int j, const uint8_t *GGML_RESTRICT q,
+                                    uint8_t *GGML_RESTRICT d,
+                                    uint8_t *GGML_RESTRICT m) {
+  if (j < 4) {
+    *d = q[j] & 63;
+    *m = q[j + 4] & 63;
+  } else {
+    *d = (q[j + 4] & 0xF) | ((q[j - 4] >> 6) << 4);
+    *m = (q[j + 4] >> 4) | ((q[j - 0] >> 6) << 4);
+  }
 }
 
 static inline int nearest_int(float fval) {
-    assert(fabsf(fval) <= 4194303.f);
-    float val = fval + 12582912.f;
-    int i; memcpy(&i, &val, sizeof(int));
-    return (i & 0x007fffff) - 0x00400000;
+  assert(fabsf(fval) <= 4194303.f);
+  float val = fval + 12582912.f;
+  int i;
+  memcpy(&i, &val, sizeof(int));
+  return (i & 0x007fffff) - 0x00400000;
 }
 
 void __nntr_quantize_row_q8_0(const _FP16 *__restrict x, void *vy, int64_t k) {
@@ -151,71 +154,77 @@ void __nntr_quantize_row_q8_0(const _FP16 *__restrict x, void *vy, int64_t k) {
 #endif
 }
 
-void __nntr_quantize_row_q8_0_ref_lossless(const float * GGML_RESTRICT x, void * GGML_RESTRICT _y, int64_t k, void * GGML_RESTRICT _y_ref) {
-    
-    assert(k % QK8_0 == 0);
-    const int nb = k / QK8_0;
+void __nntr_quantize_row_q8_0_ref_lossless(const float *GGML_RESTRICT x,
+                                           void *GGML_RESTRICT _y, int64_t k,
+                                           void *GGML_RESTRICT _y_ref) {
 
-    block_q8_0 * GGML_RESTRICT y_ref = (block_q8_0 * GGML_RESTRICT)_y_ref;
-    block_q8_0 * GGML_RESTRICT y =  (block_q8_0 * GGML_RESTRICT) _y;
+  assert(k % QK8_0 == 0);
+  const int nb = k / QK8_0;
 
-    for (int i = 0; i < nb; i++) {
-        const float d = nntr_compute_fp16_to_fp32(y_ref[i].d);
-        const float id = d ? 1.0f/d : 0.0f;
+  block_q8_0 *GGML_RESTRICT y_ref = (block_q8_0 * GGML_RESTRICT) _y_ref;
+  block_q8_0 *GGML_RESTRICT y = (block_q8_0 * GGML_RESTRICT) _y;
 
-        y[i].d = y_ref[i].d;
+  for (int i = 0; i < nb; i++) {
+    const float d = nntr_compute_fp16_to_fp32(y_ref[i].d);
+    const float id = d ? 1.0f / d : 0.0f;
 
-        for (int j = 0; j < QK8_0; ++j) {
-            const float x0 = x[i*QK8_0 + j]*id;
+    y[i].d = y_ref[i].d;
 
-            y[i].qs[j] = roundf(x0);
-        }
+    for (int j = 0; j < QK8_0; ++j) {
+      const float x0 = x[i * QK8_0 + j] * id;
+
+      y[i].qs[j] = roundf(x0);
     }
+  }
 }
 
-void __nntr_quantize_row_q4_K_ref_lossless(const float * GGML_RESTRICT x, void * GGML_RESTRICT _y, int64_t k, void * GGML_RESTRICT _y_ref) {
-    assert(k % QK_K == 0);
-    const int nb = k / QK_K;
+void __nntr_quantize_row_q4_K_ref_lossless(const float *GGML_RESTRICT x,
+                                           void *GGML_RESTRICT _y, int64_t k,
+                                           void *GGML_RESTRICT _y_ref) {
+  assert(k % QK_K == 0);
+  const int nb = k / QK_K;
 
-    block_q4_K * GGML_RESTRICT y =  (block_q4_K * GGML_RESTRICT) _y;
-    block_q4_K * GGML_RESTRICT y_ref =  (block_q4_K * GGML_RESTRICT) _y_ref;
+  block_q4_K *GGML_RESTRICT y = (block_q4_K * GGML_RESTRICT) _y;
+  block_q4_K *GGML_RESTRICT y_ref = (block_q4_K * GGML_RESTRICT) _y_ref;
 
-    uint8_t L[QK_K];
+  uint8_t L[QK_K];
 
-    for (int i = 0; i < nb; i++) {
-        for (int j = 0; j < QK_K/32; ++j) {
-            if (j < 4) {
-                y[i].scales[j] = y_ref[i].scales[j];
-                y[i].scales[j+4] = y_ref[i].scales[j+4];
-            } else {
-                y[i].scales[j+4] = y_ref[i].scales[j+4];
-                y[i].scales[j-4] |= y_ref[i].scales[j-4];
-                y[i].scales[j-0] |= y_ref[i].scales[j-0];
-            }
-        }
-        y[i].d = y_ref[i].d ;
-        y[i].dmin = y_ref[i].dmin;
-
-        uint8_t sc, m;
-        for (int j = 0; j < QK_K/32; ++j) {
-            get_scale_min_k4(j, y[i].scales, &sc, &m);
-            const float d = nntr_compute_fp16_to_fp32(y[i].d) * sc;
-            if (!d) continue;
-            const float dm = nntr_compute_fp16_to_fp32(y[i].dmin) * m;
-            for (int ii = 0; ii < 32; ++ii) {
-                int l = nearest_int((x[32*j + ii] + dm)/d);
-                l = MAX(0, MIN(15, l));
-                L[32*j + ii] = l;
-            }
-        }
-
-        uint8_t * q = y[i].qs;
-        for (int j = 0; j < QK_K; j += 64) {
-            for (int l = 0; l < 32; ++l) q[l] = L[j + l] | (L[j + l + 32] << 4);
-            q += 32;
-        }
-        x += QK_K;
+  for (int i = 0; i < nb; i++) {
+    for (int j = 0; j < QK_K / 32; ++j) {
+      if (j < 4) {
+        y[i].scales[j] = y_ref[i].scales[j];
+        y[i].scales[j + 4] = y_ref[i].scales[j + 4];
+      } else {
+        y[i].scales[j + 4] = y_ref[i].scales[j + 4];
+        y[i].scales[j - 4] |= y_ref[i].scales[j - 4];
+        y[i].scales[j - 0] |= y_ref[i].scales[j - 0];
+      }
     }
+    y[i].d = y_ref[i].d;
+    y[i].dmin = y_ref[i].dmin;
+
+    uint8_t sc, m;
+    for (int j = 0; j < QK_K / 32; ++j) {
+      get_scale_min_k4(j, y[i].scales, &sc, &m);
+      const float d = nntr_compute_fp16_to_fp32(y[i].d) * sc;
+      if (!d)
+        continue;
+      const float dm = nntr_compute_fp16_to_fp32(y[i].dmin) * m;
+      for (int ii = 0; ii < 32; ++ii) {
+        int l = nearest_int((x[32 * j + ii] + dm) / d);
+        l = MAX(0, MIN(15, l));
+        L[32 * j + ii] = l;
+      }
+    }
+
+    uint8_t *q = y[i].qs;
+    for (int j = 0; j < QK_K; j += 64) {
+      for (int l = 0; l < 32; ++l)
+        q[l] = L[j + l] | (L[j + l + 32] << 4);
+      q += 32;
+    }
+    x += QK_K;
+  }
 }
 
 size_t __nntr_quantize_q8_0(const _FP16 *src, void *dst, int64_t nrow,
@@ -244,8 +253,7 @@ void __nntr_dequantize_row_q8_0(const void *_x, _FP16 *__restrict y,
   }
 }
 
-#if 0
-void ggml_quantize_mat_q8_0_4x8(const _FP16 * GGML_RESTRICT x, void * GGML_RESTRICT vy, int64_t k) {
+void __nntr_quantize_mat_q8_0_4x8(const _FP16 * GGML_RESTRICT x, void * GGML_RESTRICT vy, int64_t k) {
     assert(QK8_0 == 32);
     assert(k % QK8_0 == 0);
     const int nb = k / QK8_0;
@@ -278,70 +286,57 @@ void ggml_quantize_mat_q8_0_4x8(const _FP16 * GGML_RESTRICT x, void * GGML_RESTR
             y[i].d[row_iter] = nntr_compute_fp32_to_fp16(d);
         }
 
-        for (int j = 0; j < 2; j++) {
-            float16x8_t v = vmulq_n_f16(srcv[0][2 * j], id[0]);
+        for (int j = 0; j < 4; j++) {
+            float16x8_t v = vmulq_n_f16(srcv[0][j], id[0]);
             int16x8_t vi = vcvtnq_s16_f16(v);
             y[i].qs[32 * j + 0] = vgetq_lane_s16(vi, 0);
             y[i].qs[32 * j + 1] = vgetq_lane_s16(vi, 1);
             y[i].qs[32 * j + 2] = vgetq_lane_s16(vi, 2);
             y[i].qs[32 * j + 3] = vgetq_lane_s16(vi, 3);
-
             y[i].qs[32 * j + 4] = vgetq_lane_s16(vi, 4);
             y[i].qs[32 * j + 5] = vgetq_lane_s16(vi, 5);
             y[i].qs[32 * j + 6] = vgetq_lane_s16(vi, 6);
             y[i].qs[32 * j + 7] = vgetq_lane_s16(vi, 7);
             
-            v = vmulq_n_f16(srcv[0][2 * j + 1], id[0]);
+            v = vmulq_n_f16(srcv[1][j], id[1]);
             vi = vcvtnq_s16_f16(v);
-            y[i].qs[32 * j + 4] = vgetq_lane_s32(vi, 0);
-            y[i].qs[32 * j + 5] = vgetq_lane_s32(vi, 1);
-            y[i].qs[32 * j + 6] = vgetq_lane_s32(vi, 2);
-            y[i].qs[32 * j + 7] = vgetq_lane_s32(vi, 3);
+            y[i].qs[32 * j + 8] = vgetq_lane_s16(vi, 0);
+            y[i].qs[32 * j + 9] = vgetq_lane_s16(vi, 1);
+            y[i].qs[32 * j + 10] = vgetq_lane_s16(vi, 2);
+            y[i].qs[32 * j + 11] = vgetq_lane_s16(vi, 3);
+            y[i].qs[32 * j + 12] = vgetq_lane_s16(vi, 4);
+            y[i].qs[32 * j + 13] = vgetq_lane_s16(vi, 5);
+            y[i].qs[32 * j + 14] = vgetq_lane_s16(vi, 6);
+            y[i].qs[32 * j + 15] = vgetq_lane_s16(vi, 7);
+            
 
-            v = vmulq_n_f16(srcv[1][2 * j], id[1]);
+            v = vmulq_n_f16(srcv[2][j], id[2]);
             vi = vcvtnq_s16_f16(v);
-            y[i].qs[32 * j + 8] = vgetq_lane_s32(vi, 0);
-            y[i].qs[32 * j + 9] = vgetq_lane_s32(vi, 1);
-            y[i].qs[32 * j + 10] = vgetq_lane_s32(vi, 2);
-            y[i].qs[32 * j + 11] = vgetq_lane_s32(vi, 3);
-            v = vmulq_n_f16(srcv[1][2 * j + 1], id[1]);
-            vi = vcvtnq_s16_f16(v);
-            y[i].qs[32 * j + 12] = vgetq_lane_s32(vi, 0);
-            y[i].qs[32 * j + 13] = vgetq_lane_s32(vi, 1);
-            y[i].qs[32 * j + 14] = vgetq_lane_s32(vi, 2);
-            y[i].qs[32 * j + 15] = vgetq_lane_s32(vi, 3);
+            y[i].qs[32 * j + 16] = vgetq_lane_s16(vi, 0);
+            y[i].qs[32 * j + 17] = vgetq_lane_s16(vi, 1);
+            y[i].qs[32 * j + 18] = vgetq_lane_s16(vi, 2);
+            y[i].qs[32 * j + 19] = vgetq_lane_s16(vi, 3);
+            y[i].qs[32 * j + 20] = vgetq_lane_s16(vi, 4);
+            y[i].qs[32 * j + 21] = vgetq_lane_s16(vi, 5);
+            y[i].qs[32 * j + 22] = vgetq_lane_s16(vi, 6);
+            y[i].qs[32 * j + 23] = vgetq_lane_s16(vi, 7);
 
-            v = vmulq_n_f16(srcv[2][2 * j], id[2]);
+            v = vmulq_n_f16(srcv[3][j], id[3]);
             vi = vcvtnq_s16_f16(v);
-            y[i].qs[32 * j + 16] = vgetq_lane_s32(vi, 0);
-            y[i].qs[32 * j + 17] = vgetq_lane_s32(vi, 1);
-            y[i].qs[32 * j + 18] = vgetq_lane_s32(vi, 2);
-            y[i].qs[32 * j + 19] = vgetq_lane_s32(vi, 3);
-            v = vmulq_n_f16(srcv[2][2 * j + 1], id[2]);
-            vi = vcvtnq_s16_f16(v);
-            y[i].qs[32 * j + 20] = vgetq_lane_s32(vi, 0);
-            y[i].qs[32 * j + 21] = vgetq_lane_s32(vi, 1);
-            y[i].qs[32 * j + 22] = vgetq_lane_s32(vi, 2);
-            y[i].qs[32 * j + 23] = vgetq_lane_s32(vi, 3);
-
-            v = vmulq_n_f16(srcv[3][2 * j], id[3]);
-            vi = vcvtnq_s16_f16(v);
-            y[i].qs[32 * j + 24] = vgetq_lane_s32(vi, 0);
-            y[i].qs[32 * j + 25] = vgetq_lane_s32(vi, 1);
-            y[i].qs[32 * j + 26] = vgetq_lane_s32(vi, 2);
-            y[i].qs[32 * j + 27] = vgetq_lane_s32(vi, 3);
-            v = vmulq_n_f16(srcv[3][2 * j + 1], id[3]);
-            vi = vcvtnq_s16_f16(v);
-            y[i].qs[32 * j + 28] = vgetq_lane_s32(vi, 0);
-            y[i].qs[32 * j + 29] = vgetq_lane_s32(vi, 1);
-            y[i].qs[32 * j + 30] = vgetq_lane_s32(vi, 2);
-            y[i].qs[32 * j + 31] = vgetq_lane_s32(vi, 3);
+            y[i].qs[32 * j + 24] = vgetq_lane_s16(vi, 0);
+            y[i].qs[32 * j + 25] = vgetq_lane_s16(vi, 1);
+            y[i].qs[32 * j + 26] = vgetq_lane_s16(vi, 2);
+            y[i].qs[32 * j + 27] = vgetq_lane_s16(vi, 3);
+            y[i].qs[32 * j + 28] = vgetq_lane_s16(vi, 4);
+            y[i].qs[32 * j + 29] = vgetq_lane_s16(vi, 5);
+            y[i].qs[32 * j + 30] = vgetq_lane_s16(vi, 6);
+            y[i].qs[32 * j + 31] = vgetq_lane_s16(vi, 7);
         }
     }
 #else
     // scalar
     const int blck_size_interleave = 8;
-    float srcv[4][QK8_0];
+    _FP16 srcv[4][QK8_0];
     float id[4];
 
     for (int i = 0; i < nb; i++) {
@@ -370,6 +365,5 @@ void ggml_quantize_mat_q8_0_4x8(const _FP16 * GGML_RESTRICT x, void * GGML_RESTR
     }
 #endif
 }
-#endif
 
 } // namespace nntrainer
